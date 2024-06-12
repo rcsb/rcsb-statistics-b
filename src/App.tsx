21f7d2f2b8805771f08a339c91958ce92c35a600
Ronald Brown <ronald.brown@rcsb.org>
2024-06-12 13:17:30 -0400

    adding lazy loading to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage, DistSourceOrgNat } from './pages';
 
-function App() {
+const HomePage = React.lazy(() => import('./pages/home'));
+const DistSourceOrgNat = React.lazy(() => import('./pages/dist-source-org-nat'));
 
-  const routes = [
-    { path: "/", component: HomePage },
-    { path: "/distribution-source-organism-natural", component: DistSourceOrgNat },
-  ];
+const routes = [
+  { path: "/", component: HomePage },
+  { path: "/distribution-source-organism-natural", component: DistSourceOrgNat },
+];
 
-
+const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-      {routes.map((route, index) => (
-        <Route key={index} path={route.path} element={<route.component />} />
-      ))}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={<route.component />} />
+          ))}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
