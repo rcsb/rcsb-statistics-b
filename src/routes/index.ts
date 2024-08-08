96cdb8e710d4c4227040a044057e7dfd3d9e1eb0
Ronald Brown <ronald.brown@rcsb.org>
2024-08-08 16:50:56 -0400

    route config stuff

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,8 @@ const routes = [
   { path: paths.HOME, element: lazy(() => import('../pages/Home')), exact: true },
   { path: paths.PDB_DATA_GROWTH, element: lazy(() => import('../pages/PdbDataGrowth')) },
   { path: paths.PDB_DATA_DISTRIBUTION, element: lazy(() => import('../pages/PdbDataDistribution')) },
-  { path: paths.OTHER_STATISTICS, element: lazy(() => import('../pages/other-stats')) },
-  { path: paths.PDB_DATA_SNAPSHOT, element: lazy(() => import('../pages/pdb-data-snapshot')) }
+  { path: paths.OTHER_STATISTICS, element: lazy(() => import('../pages/OtherStats')) },
+  { path: paths.PDB_DATA_SNAPSHOT, element: lazy(() => import('../pages/PdbDataSnapshot')) }
 ];
 
 export default routes;
